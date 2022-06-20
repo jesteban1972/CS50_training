8c3d343383f46293d05312135ce4398b15a75a72
javier.esteban <javier.esteban@depau.com>
2022-06-20 10:44:03 +0200

    some changes in config files

--- CS50_Python_JS_ud6/addition/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "addition"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
--- CS50_Python_JS_ud6/react/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "react"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-]
